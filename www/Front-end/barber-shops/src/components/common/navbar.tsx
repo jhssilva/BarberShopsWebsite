c54cf1bf1770ba308ba80b176112c460c1458617
jose.silva <jhssilva96@gmail.com>
2021-07-01 14:52:16 +0100

    Navbar updated

--- www/Front-end/barber-shops/src/components/common/navbar.tsx
+++ www/Front-end/barber-shops/src/components/common/navbar.tsx
@@ -6,7 +6,26 @@ export interface NavBarProps {}
 export interface NavBarState {}
 
 const NavBar = (props: any) => {
-  const state = { menu: ["Find Me", "About US", "Login"] };
+  const state = {
+    menu: [
+      { name: "Home", isActive: true, path: "/" },
+      { name: "Login", isActive: false, path: "/login" },
+    ],
+  };
+
+  const NavBarItem = (item: any) => {
+    console.log(item);
+    return (
+      <Link
+        key={item.name}
+        to={item.path}
+        className={"nav-link " + (item.isActive ? "active" : "")}
+      >
+        {item.name}
+      </Link>
+    );
+  };
+
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
@@ -30,72 +49,9 @@ const NavBar = (props: any) => {
             id="navbarSupportedContent"
           >
             <ul className="navbar-nav mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-
-              {state.menu.map((entry) => (
-                <li key="tag" className="nav-item">
-                  <Link className="nav-link" to="/login">
-                    {entry}
-                  </Link>
-                </li>
-              ))}
-
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Find
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Login
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  href="#"
-                  tabIndex={-1}
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
+              {state.menu.map((item) => {
+                return <li className="nav-item">{NavBarItem(item)}</li>;
+              })}
             </ul>
           </div>
         </div>
